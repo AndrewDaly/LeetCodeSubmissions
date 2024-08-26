1b42913569d57071fe72905962c7eccef88507c5
Andrew Daly <andrew123daly@gmail.com>
2024-08-25 21:11:06 -0400

    better solution

--- best_time_to_buy_and_sell.py
+++ best_time_to_buy_and_sell.py
@@ -9,6 +9,10 @@ class Solution(object):
         biggest_remaining_element = prices[0]
         for i, j in enumerate(prices):
             if i == 0:
+                biggest_remaining_element = max(prices[i:len(prices)])
+                current_profit = biggest_remaining_element - lowest_element_so_far
+                if current_profit > max_profit:
+                    max_profit = current_profit    
                 continue
             lowest_element_so_far = min(prices[0:i])
             biggest_remaining_element = max(prices[i:len(prices)])
